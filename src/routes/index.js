a525bd45b9b5c95c87dad038ce12e0cc1e970959
shailajakalai <121797891+shailajakalai@users.noreply.github.com>
2023-05-10 14:41:14 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 
 router.get('/', (request, response, next) => {
-    response.status(200).send("Hello World!");
+    response.status(200).send("HashTek Solutions-Training");
 });
 
 module.exports = router;
